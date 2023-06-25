e3d3c436a539933c195be14f1e1c40a27a098676
ybw0014 <i@guizhanss.com>
2023-06-25 18:12:00 -0400

    更新前置提醒信息

--- src/main/java/dev/j3fftw/litexpansion/LiteXpansion.java
+++ src/main/java/dev/j3fftw/litexpansion/LiteXpansion.java
@@ -30,6 +30,13 @@ public class LiteXpansion extends JavaPlugin implements SlimefunAddon {
     public void onEnable() {
         setInstance(this);
 
+        if (!getServer().getPluginManager().isPluginEnabled("GuizhanLibPlugin")) {
+            getLogger().log(Level.SEVERE, "本插件需要 鬼斩前置库插件(GuizhanLibPlugin) 才能运行!");
+            getLogger().log(Level.SEVERE, "从此处下载: https://50l.cc/gzlib");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
         if (!new File(getDataFolder(), "config.yml").exists()) {
             saveDefaultConfig();
         }
